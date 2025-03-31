4ddcb2668c320b2bc35943929438c1dc1424da66
miquelLlorca <mlb51@gcloud.ua.es>
2025-03-31 12:50:46 +0200

    First commit

---
+++ missions.js
@@ -0,0 +1,6 @@
+let missions = [
+    { id: 1, name: "Score 100 points", objective: 100, progress: 0, reward: 50, stat:'highScore' },
+    { id: 2, name: "Clear 5 rows", objective: 5, progress: 0, reward: 30, stat:'rowsCleared' },
+    { id: 3, name: "Buy an upgrade", objective: 1, progress: 0, reward: 20, stat:'upgradesBought' },
+];
+export default missions;
